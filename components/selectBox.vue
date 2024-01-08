b0ed8cdab1aeb8a58429b40145d3c6aeb6308dc6
leejeongkang <krj1226@gmail.com>
2024-01-08 11:37:46 +0900

    setup style로 코드 리팩토링

--- components/selectBox.vue
+++ components/selectBox.vue
@@ -1,36 +1,32 @@
 // src/components/SelectBox.vue
 <template>
-  <div style="width: 300px; height: 30px;">
-    <button @click="toggleList" style="width: 100%; height: 100%">{{ selectedLabel }}</button>
-    <ul v-show="isShowBox" style="position: absolute; z-index: 100;">
-      <li v-for="option in options" :key="option[valueKey]" @click="handleSelect(option)">
+  <div style="width: 300px; height: 30px">
+    <button @click="toggleList" style="width: 100%; height: 100%">
+      {{ selectedLabel }}
+    </button>
+    <ul v-show="isShowBox" style="position: absolute; z-index: 100">
+      <li
+        v-for="option in options"
+        :key="option[valueKey]"
+        @click="handleSelect(option)"
+      >
         {{ option[labelKey] }}
       </li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { selectComposition } from '@/components/composables/selectComposition.ts';
+<script lang="ts" setup>
+import { defineEmits, defineProps } from "vue";
+import { selectComposition } from "@/components/composables/selectComposition.ts";
+const props = defineProps(["labelKey", "valueKey", "disabled", "options"]);
+const emit = defineEmits(["select"]);
 
-export default defineComponent({
-  props: ['labelKey', 'valueKey', 'disabled', 'options'],
-  setup(props, { emit }) {
-    const { selectedLabel, selectedValue, handleSelect, toggleList, isShowBox } = selectComposition({
-      ...props,  // props를 넘겨줌
-      onSelect: (value: string | number) => {
-        emit("select", value);
-      },
-    });
-
-    return {
-      selectedLabel,
-      selectedValue,
-      handleSelect,
-      toggleList,
-      isShowBox,
-    };
-  },
-});
+const { selectedLabel, selectedValue, handleSelect, toggleList, isShowBox } =
+  selectComposition({
+    ...props, // props를 넘겨줌
+    onSelect: (value: string | number) => {
+      emit("select", value);
+    },
+  });
 </script>
