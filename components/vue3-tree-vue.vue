a0538e337a56e018e342a9bbe8498efadb0ded55
leejeongkang <krj1226@gmail.com>
2024-02-19 18:32:39 +0900

    feat: vue3-tree-vue sample code

---
+++ components/vue3-tree-vue.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <Vue3TreeVue
+      :items="items"
+      :isCheckable="true"
+      :hideGuideLines="false"
+      @onCheck="onItemChecked"
+      @onSelect="onItemSelected"
+    >
+      <template v-slot:item-prepend-icon="treeViewItem">
+        <span class="icon">📂</span>
+      </template>
+      <template v-slot:item-prepend="treeViewItem"
+        ><span class="icon"></span
+      ></template>
+      <template v-slot:item-append="treeViewItem">
+        <span v-if="treeViewItem.connect"
+          >(연결상태: {{ treeViewItem.connect }})</span
+        >
+        &nbsp&nbsp&nbsp&nbsp&nbsp
+        <span v-if="treeViewItem.user">(소유자: {{ treeViewItem.user }})</span>
+      </template>
+    </Vue3TreeVue>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Vue3TreeVue from "vue3-tree-vue";
+import "vue3-tree-vue/dist/style.css";
+
+type TreeViewItem = {
+  name: string;
+  id?: string | number;
+  children?: TreeViewItem[];
+  checked?: boolean;
+  selected?: boolean;
+  expanded?: boolean;
+  disabled?: boolean; // When disabled, an item can neither be selected or checked
+  meta?: any; // provides meta-data of any type per node.
+};
+const onItemChecked = (checkedItems: []) => console.log(checkedItems);
+const onItemSelected = (item: TreeViewItem) => console.log(item);
+
+/**
+ * 트리 노드 추가, 삭제, 이름 변경은 내가 직접 구현..
+ */
+// const onExpanded = (expandedItem: TreeViewItem) => {
+//   //fetch data
+//   const lazyLoadedItems = fetchFromApi(...);
+//   expandedItem.children.push(...lazyLoadedItems)
+// }
+const items = ref([
+  {
+    name: "Node1",
+    children: [{ name: "node1-1", user: "리드미" }],
+    connect: "연결",
+    user: "강이정",
+  },
+  { name: "Node2", user: "강이정" },
+]);
+</script>
